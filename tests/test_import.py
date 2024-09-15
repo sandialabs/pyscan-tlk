486399fd9c5c4d0ee07cb41bd1a606e0128e116e
amounce <amounce@gmail.com>
2024-09-15 15:10:06 -0600

    fix: fixed version name

--- tests/test_import.py
+++ tests/test_import.py
@@ -1,37 +1,37 @@
-from tlk.benchtopbrushlessmotor import *
-from tlk.benchtopdcservo import *
-from tlk.benchtopnanotrack import *
-from tlk.benchtoppiezo import *
-from tlk.benchtoppiezopdxc2 import *
-from tlk.benchtopprecisionpiezo import *
-from tlk.benchtopsteppermotor import *
-# from tlk.benchtopvoicecoil import *
-from tlk.filterflipper import *
-from tlk.integratedprecisionpiezo import *
-from tlk.integratedsteppermotor import *
-from tlk.kcubebrushlessmotor import *
-# from tlk.kcubedcservo import *
-from tlk.kcubeinertialmotor import *
-from tlk.kcubelaserdiode import *
-from tlk.kcubelasersource import *
-from tlk.kcubenanotrack import *
-from tlk.kcubepiezo import *
-from tlk.kcubepiezostraingauge import *
-from tlk.kcubepositionaligner import *
-from tlk.kcubesolenoid import *
-from tlk.kcubesteppermotor import *
-# from tlk.modularnanotrack import *
-# from tlk.modularsteppermotor import *
-from tlk.polarizer import *
-from tlk.tcubebrushlessmotor import *
-from tlk.tcubeinertialmotor import *
-from tlk.tcubelaserdiode import *
-from tlk.tcubelasersource import *
-from tlk.tcubenanotrack import *
-from tlk.tcubepiezo import *
-from tlk.tcubequad import *
-from tlk.tcubeservo import *
-from tlk.tcubesolenoid import *
-from tlk.tcubesteppermotor import *
-from tlk.tcubestraingauge import *
-from tlk.tcubetec import *
+from pyscan_tlk.benchtopbrushlessmotor import *
+from pyscan_tlk.benchtopdcservo import *
+from pyscan_tlk.benchtopnanotrack import *
+from pyscan_tlk.benchtoppiezo import *
+from pyscan_tlk.benchtoppiezopdxc2 import *
+from pyscan_tlk.benchtopprecisionpiezo import *
+from pyscan_tlk.benchtopsteppermotor import *
+# from pyscan_tlk.benchtopvoicecoil import *
+from pyscan_tlk.filterflipper import *
+from pyscan_tlk.integratedprecisionpiezo import *
+from pyscan_tlk.integratedsteppermotor import *
+from pyscan_tlk.kcubebrushlessmotor import *
+# from pyscan_tlk.kcubedcservo import *
+from pyscan_tlk.kcubeinertialmotor import *
+from pyscan_tlk.kcubelaserdiode import *
+from pyscan_tlk.kcubelasersource import *
+from pyscan_tlk.kcubenanotrack import *
+from pyscan_tlk.kcubepiezo import *
+from pyscan_tlk.kcubepiezostraingauge import *
+from pyscan_tlk.kcubepositionaligner import *
+from pyscan_tlk.kcubesolenoid import *
+from pyscan_tlk.kcubesteppermotor import *
+# from pyscan_tlk.modularnanotrack import *
+# from pyscan_tlk.modularsteppermotor import *
+from pyscan_tlk.polarizer import *
+from pyscan_tlk.tcubebrushlessmotor import *
+from pyscan_tlk.tcubeinertialmotor import *
+from pyscan_tlk.tcubelaserdiode import *
+from pyscan_tlk.tcubelasersource import *
+from pyscan_tlk.tcubenanotrack import *
+from pyscan_tlk.tcubepiezo import *
+from pyscan_tlk.tcubequad import *
+from pyscan_tlk.tcubeservo import *
+from pyscan_tlk.tcubesolenoid import *
+from pyscan_tlk.tcubesteppermotor import *
+from pyscan_tlk.tcubestraingauge import *
+from pyscan_tlk.tcubetec import *
