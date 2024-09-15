16e5727bd18050691b88a5caf8536c60f5a65e09
amounce <amounce@gmail.com>
2024-09-15 15:17:56 -0600

    Revert "fix: versioning fixed"
    This reverts commit 8453d1f3ae4602251b944a755b58edd11653a7e8.

--- src/pyscan_tlk/__init__.py
+++
@@ -1,9 +0,0 @@
-import importlib.resources
-import json
-import os
-
-
-dir = os.path.dirname(os.path.abspath(__file__))
-
-with importlib.resources.open_text("pyscan_tlk", "version.json") as file:
-    __version__ = json.load(file)['version']
